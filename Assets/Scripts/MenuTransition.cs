0d43a69e00be09da4b74d87553aa3465f4cee738
Unknown <thekings.stephen@gmail.com>
2018-08-21 16:13:12 +0930

    Made Menu Elements Snappable for animations
    Simply set time to zero

--- Assets/Scripts/MenuTransition.cs
+++ Assets/Scripts/MenuTransition.cs
@@ -44,8 +44,13 @@ public class MenuTransition : MonoBehaviour {
 		foreach (Transition tran in m_outTransitions)
 			if (tran.ObjectToAnimate.Obj)
 			{
-				tweeners.Add(tran.ObjectToAnimate.Obj.transform.DOLocalMove(tran.ObjectToAnimate.TargetPosition, tran.ObjectToAnimate.Time));
-				tweeners.Add(tran.ObjectToAnimate.Obj.transform.DOScale(tran.ObjectToAnimate.TargetScale, tran.ObjectToAnimate.Time));
+				if (tran.ObjectToAnimate.Time == 0)
+					tran.ObjectToAnimate.Obj.transform.position = tran.ObjectToAnimate.TargetPosition;
+				else
+				{
+					tweeners.Add(tran.ObjectToAnimate.Obj.transform.DOLocalMove(tran.ObjectToAnimate.TargetPosition, tran.ObjectToAnimate.Time));
+					tweeners.Add(tran.ObjectToAnimate.Obj.transform.DOScale(tran.ObjectToAnimate.TargetScale, tran.ObjectToAnimate.Time));
+				}
 			}
 
 			foreach (var tween in tweeners)
@@ -76,8 +81,13 @@ public class MenuTransition : MonoBehaviour {
 				tran.ObjectToToggleEnabled.SetActive(true);
 			if (tran.ObjectToAnimate.Obj)
 			{
-				tran.ObjectToAnimate.Obj.transform.DOLocalMove(tran.ObjectToAnimate.TargetPosition, tran.ObjectToAnimate.Time);
-				tran.ObjectToAnimate.Obj.transform.DOScale(tran.ObjectToAnimate.TargetScale, tran.ObjectToAnimate.Time);
+				if (tran.ObjectToAnimate.Time == 0)
+					tran.ObjectToAnimate.Obj.transform.position = tran.ObjectToAnimate.TargetPosition;
+				else
+				{
+					tran.ObjectToAnimate.Obj.transform.DOLocalMove(tran.ObjectToAnimate.TargetPosition, tran.ObjectToAnimate.Time);
+					tran.ObjectToAnimate.Obj.transform.DOScale(tran.ObjectToAnimate.TargetScale, tran.ObjectToAnimate.Time);
+				}
 			}
 		}
 
