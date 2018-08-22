45e99951ed4f45d197a3fd6933fb2dd11937b1f0
Unknown <thekings.stephen@gmail.com>
2018-08-22 11:11:56 +0930

    Added WIP Level gen scrips. do not use them yet

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeatRider
+{
+	public class LevelGenerator : MonoBehaviour
+	{
+		[Tooltip("A scriptable object that determines the layout of a level")]
+		LevelTemplate m_levelTemplate;
+
+		PickupSpawner m_PickupSpawner;
+
+		// Use this for initialization
+		void Start()
+		{
+			m_PickupSpawner = FindObjectOfType<PickupSpawner>();
+		}
+
+		void CreatePool()
+		{
+
+		}
+
+		void CreateLayerOfLevel()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
