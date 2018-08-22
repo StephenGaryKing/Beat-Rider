e30a44b61ecb03121169d295d223b252e2c8d6c3
Unknown <thekings.stephen@gmail.com>
2018-08-22 12:10:48 +0930

    Still working on Level Gen scrips
    Sorry for clogging up the feed. I'm just scared to ad a branch.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 namespace BeatRider
 {
+	[UnityEditor.InitializeOnLoad]
+	class EditorUpdator
+	{
+		static EditorUpdator()
+		{
+			UnityEditor.EditorApplication.update += Update;
+		}
+
+		static void Update()
+		{
+			//update percentages of spawn rates
+		}
+	}
+
 	public class LevelGenerator : MonoBehaviour
 	{
 		[Tooltip("A scriptable object that determines the layout of a level")]
@@ -19,12 +33,31 @@ namespace BeatRider
 
 		void CreatePool()
 		{
-
+			// Make a pool of objects to make each layer out of
 		}
 
 		void CreateLayerOfLevel()
 		{
+			// Make a layer of the level using the objects in the pools
+			Vector3 m_spawnLocation = Vector3.forward * m_levelTemplate.m_unitsAwayToSpawn * m_levelTemplate.m_unitSize;
 
+			switch (m_levelTemplate.m_levelGenerationType)
+			{
+				case (LevelType.GRID):
+					//create Track piece
+					foreach(var track in m_levelTemplate.m_trackElements)
+					{
+						//add the values together then pick a random value via roulette wheels selection
+
+					}
+					//for each scenery layer, create a piece of scenery
+
+
+					break;
+				case (LevelType.RANDOM):
+
+					break;
+			}
 		}
 
 		// Update is called once per frame
--- Assets/Scripts/LevelTemplate.cs
+++ Assets/Scripts/LevelTemplate.cs
@@ -4,36 +4,40 @@ using UnityEngine;
 
 namespace BeatRider
 {
-	enum LevelType
+	public enum LevelType
 	{
 		GRID,
 		RANDOM
 	}
 
 	[System.Serializable]
-	struct SceneryElement
+	public struct SceneryElement
 	{
 		public float spawnChanceWeight;
 		public Vector2 randomScaleVariance;
+		[Header("Info")]
+		public float spawnChancePercentage;
 	}
 
 	[System.Serializable]
-	struct TrackElement
+	public struct TrackElement
 	{
 		public float spawnChanceWeight;
+		[Header("Info")]
+		public float spawnChancePercentage;
 	}
 
 	[CreateAssetMenu(fileName = "LevelTemplate", menuName = "Beat Rider/LevelTemplate", order = 1)]
-	public class LevelTemplate : MonoBehaviour
+	public class LevelTemplate : ScriptableObject
 	{
-		LevelType m_levelGenerationType;
-		float m_unitSize;
-		int m_numOfSceneryLayers;
-		List<SceneryElement> m_sceneryElements = new List<SceneryElement>();
-		List<TrackElement> m_trackElements = new List<TrackElement>();
+		public LevelType m_levelGenerationType;
+		public float m_unitSize;
+		public int m_numOfSceneryLayers;
+		public List<SceneryElement> m_sceneryElements = new List<SceneryElement>();
+		public List<TrackElement> m_trackElements = new List<TrackElement>();
 		[Tooltip("Bumber of units in front of the player to spawn the level")]
-		int m_unitsAwayToSpawn = 10;
+		public int m_unitsAwayToSpawn = 10;
 		[Tooltip("The time taken for the level to reach the player in seconds")]
-		float m_travelTime = 2;
+		public float m_travelTime = 2;
 	}
 }
