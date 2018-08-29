438e33bef8c198369c318a5e28755e42742c0fe3
Travis Norton <nortj003@outlook.com>
2018-08-29 11:07:17 +0930

    Updated boost pads and UI

---
+++ Assets/Scripts/FlashImage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashImage : MonoBehaviour {
+
+    Image m_image;
+    public float m_flashSpeed = 1;
+
+    float m_timer = 0;
+
+	// Use this for initialization
+	void Start () {
+        m_image = GetComponent<Image>();
+	}
+
+    private void Update()
+    {
+        m_timer += Time.deltaTime;
+        if (m_timer >= m_flashSpeed)
+        {
+            ChangeViz();
+            m_timer = 0;
+        }
+    }
+
+    void ChangeViz()
+    {
+        Color col = Color.white;
+        col.a = (m_image.color.a == 1)? 0 : 1;
+        m_image.color = col;
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,19 +5,26 @@ using MusicalGameplayMechanics;
 
 public class GameController : MonoBehaviour {
 
-
     public SongController songcontroller;		// used when pausing the song
     [SerializeField] GameObject pauseMenu;		// displayed when the game is paused
 
-	void Update () {
 		// Pause the game
+    [SerializeField] GameObject m_pauseMenu;
+    [SerializeField] GameObject m_playButton;
+    [SerializeField] GameObject m_pauseButton;
+	
+	// Update is called once per frame
+	void Update () {
+        CursorControl();
         if(Input.GetKeyDown(KeyCode.Escape) && songcontroller.m_paused == false)
         {
             songcontroller.Pause();
             if (songcontroller.m_paused)
             {
                 Time.timeScale = 0;
-                pauseMenu.SetActive(true);
+                m_pauseMenu.SetActive(true);
+                m_pauseButton.SetActive(true);
+                m_playButton.SetActive(false);
             }
         }
 		// Play the game from a paused state
@@ -37,13 +44,32 @@ public class GameController : MonoBehaviour {
 	public void ReturnToGame()
     {
         songcontroller.UnPause();
-        pauseMenu.SetActive(false);
+        m_pauseMenu.SetActive(false);
+        m_pauseButton.SetActive(false);
+        m_playButton.SetActive(true);
         Time.timeScale = 1;
     }
 
 	/// <summary>
 	/// Exit the game or stop running if in editor
 	/// </summary>
+    private void CursorControl()
+    {
+        if(songcontroller.m_songIsBeingPlayed == true)
+        {
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.visible = true;
+        }
+
+        if (songcontroller.m_paused == true && Time.timeScale == 0 && songcontroller.m_songIsBeingPlayed == true)
+            Cursor.visible = true;
+
+        else if (songcontroller.m_paused == false && Time.timeScale == 1 && songcontroller.m_songIsBeingPlayed == true)
+            Cursor.visible = false;
+    }
     public void ExitGame()
     {
 #if UNITY_STANDALONE
