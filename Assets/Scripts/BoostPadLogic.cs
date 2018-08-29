de25a77b2f32c25216f4d9f2d7cdd89da13d5c2d
Unknown <thekings.stephen@gmail.com>
2018-08-29 10:31:59 +0930

    Added Diegetic UI to boost pads

--- Assets/Scripts/BoostPadLogic.cs
+++ Assets/Scripts/BoostPadLogic.cs
@@ -2,8 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoostPadLogic : MonoBehaviour {
+namespace BeatRider
+{
+	public class BoostPadLogic : MonoBehaviour
+	{
 
-	// this value is used when the player hits the boost pad
-	public float m_boostAmount = 10;
+		// this value is used when the player hits the boost pad
+		public float m_boostAmount = 10;
+		public Renderer[] m_arrows;
+		public float m_maxEmission;
+		public float m_minEmission;
+		public bool m_invertLight = false;
+
+		float m_maxVal;
+		Color m_arrowColour;
+		PlayerController m_player;
+
+		private void Start()
+		{
+			m_player = FindObjectOfType<PlayerController>();
+			if (m_arrows.Length != 0)
+				m_arrowColour = m_arrows[0].material.GetColor("_EmissionColor");
+			m_maxVal = m_player.m_maxFOV - m_player.m_minFOV;
+		}
+
+		private void Update()
+		{
+			// scale the lighting of the boost pads with the speed travelling
+			// find the percentage of max speed
+			float speedPercent = (m_player.m_targetFOV - m_player.m_minFOV) / (m_maxVal - (m_player.m_maxFOV - m_player.m_minFOV) / 5);
+			for (int i = 0; i < m_arrows.Length; i++)
+				UpdateArrow(i, speedPercent);
+		}
+
+		private void UpdateArrow(int i, float speedPercent)
+		{
+			// find the percentage of the current arrow through the list of arrows
+			float percentThroughList = (i + 1) / (float)m_arrows.Length;
+			Debug.Log("speed : " + speedPercent + " [] list : " + percentThroughList);
+
+			if (percentThroughList <= speedPercent)
+				m_arrows[(m_invertLight) ? m_arrows.Length - 1 - i : i].material.SetColor("_EmissionColor", m_arrowColour * ((m_invertLight) ? m_minEmission : m_maxEmission));
+			else
+				m_arrows[(m_invertLight) ? m_arrows.Length - 1 - i : i].material.SetColor("_EmissionColor", m_arrowColour * ((m_invertLight) ? m_maxEmission : m_minEmission));
+		}
+	}
 }
