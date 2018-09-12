552d12403d134ce60ff7b91fbf65154da4c4935b
Travis Norton <nortj003@outlook.com>
2018-09-12 11:46:32 +0930

    Updated menus

---
+++ Assets/Scripts/UI/TextSizeChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSizeChange : MonoBehaviour {
+
+    Vector3 m_savedScale;
+    public float m_scaleAdjust = 2;
+
+    public void Start()
+    {
+        m_savedScale = transform.localScale;
+    }
+
+    public void EnlargeText()
+    {
+        transform.localScale = m_savedScale * m_scaleAdjust;
+    }
+    public void ShrinkText()
+    {
+        transform.localScale = m_savedScale;
+    }
+
+}
