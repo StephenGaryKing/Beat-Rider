f0d48273e71294d0ff00080c4ab94ca9bdae10b2
Unknown <thekings.stephen@gmail.com>
2018-09-17 14:37:45 +0930

    Finished setting up Ship Customisation (for colours at least)

--- Assets/Scripts/Customisation/CustomisationButton.cs
+++ Assets/Scripts/Customisation/CustomisationButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum PartToCustomise
 {
@@ -10,16 +11,28 @@ public enum PartToCustomise
 	Trail
 }
 
+[RequireComponent(typeof(Toggle))]
 public class CustomisationButton : MonoBehaviour {
 
 	public PartToCustomise m_partToCustomise;
 	[HideInInspector] public Unlockable m_unlockable;
-	ShipCustomiser m_shipCustomiser;
+	public ShipCustomiser m_shipCustomiser;
+	Toggle m_toggle;
 
 	// Use this for initialization
 	void Start()
 	{
+		m_toggle = GetComponent<Toggle>();
 		m_shipCustomiser = FindObjectOfType<ShipCustomiser>();
+	}
+
+	public void SetUnlockable(Unlockable unlock)
+	{
+		m_unlockable = unlock;
+		Image image = GetComponent<Image>();
+		image.sprite = m_unlockable.m_icon;
+		if (m_partToCustomise == PartToCustomise.Colour || m_partToCustomise == PartToCustomise.Highlight)
+			image.color = (m_unlockable as UnlockableColour).m_colour;
 	}
 
 	public void ApplyCustomisation()
@@ -27,22 +40,17 @@ public class CustomisationButton : MonoBehaviour {
 		switch (m_partToCustomise)
 		{
 			case (PartToCustomise.Colour):
-				//m_shipCustomiser.CustomiseColour(m_unlockable as UnlockableColour);
+				m_shipCustomiser.CustomiseColour(m_unlockable as UnlockableColour, m_toggle.isOn);
 				break;
 			case (PartToCustomise.Highlight):
-				//m_shipCustomiser.CustomiseHighlights(m_unlockable as UnlockableColour);
+				m_shipCustomiser.CustomiseHighlights(m_unlockable as UnlockableColour, m_toggle.isOn);
 				break;
 			case (PartToCustomise.Ship):
-				m_shipCustomiser.CustomiseShip(m_unlockable as UnlockableShip);
+				m_shipCustomiser.CustomiseShip(m_unlockable as UnlockableShip, m_toggle.isOn);
 				break;
 			case (PartToCustomise.Trail):
-				m_shipCustomiser.CustomiseTrail(m_unlockable as UnlockableTrail);
+				m_shipCustomiser.CustomiseTrail(m_unlockable as UnlockableTrail, m_toggle.isOn);
 				break;
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
