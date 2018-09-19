6217f9534470701682d065e063f36dc74d74642f
Travis Norton <nortj003@outlook.com>
2018-09-19 11:52:25 +0930

    Added UI Particles

---
+++ Assets/Scripts/UI/ParticleStudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleStudio : MonoBehaviour {
+
+    // singleton class that stores parameters from the particle studio camera to 
+    public static ParticleStudio inst;
+    public int textureWidth;
+    public float cameraWidth;
+
+    void Start()
+    {
+        inst = this;
+        // get the camera
+        Camera cam = GetComponent<Camera>();
+        if (cam && cam.targetTexture)
+        {
+            textureWidth = cam.targetTexture.width;
+            cameraWidth = cam.orthographicSize * 2;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIParticle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIParticle : MonoBehaviour {
+
+    public GameObject prefab;
+    public GameObject instance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float textureWidth = ParticleStudio.inst.textureWidth;
+        float cameraWidth = ParticleStudio.inst.cameraWidth;
+
+        // update the position to match the orthographic camera, so the particles in the RawImage sit over the component
+        if (instance)
+            instance.transform.position = this.transform.localPosition * cameraWidth / textureWidth;
+    }
+
+    public void Show()
+    {
+        if (instance == null)
+            instance = Instantiate(prefab);
+
+        // toggle on and off to restart particle system (and whatever else)
+        instance.SetActive(false);
+        instance.SetActive(true);
+    }
+
+}
