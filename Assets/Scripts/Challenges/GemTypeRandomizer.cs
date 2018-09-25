f9928905b3ceb207398cd2d3db707e339db84b36
Unknown <thekings.stephen@gmail.com>
2018-09-25 14:36:23 +0930

    Added Canyon models and implemented crafting
    Crafting still required UI implementation to change target gem and display recipe at runtime

---
+++ Assets/Scripts/Challenges/GemTypeRandomizer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeatRider
+{
+	public class GemTypeRandomizer : MonoBehaviour
+	{
+		ChallengeManager m_challengeManager;
+		[HideInInspector] public int m_gemIndexNumber;
+		Renderer m_ren;
+
+		// Use this for initialization
+		void Awake()
+		{
+			m_challengeManager = FindObjectOfType<ChallengeManager>();
+			m_ren = GetComponent<Renderer>();
+		}
+
+		public Gem GetGem()
+		{
+			return m_challengeManager.m_allChallenges[m_gemIndexNumber];
+		}
+
+		private void OnEnable()
+		{
+			Randomize();
+			ApplyMaterial();
+		}
+
+		void Randomize()
+		{
+			if (m_challengeManager.m_challengesCompleated.Count > 0)
+			{
+				//NOTE: maybe implement a roulett wheel to allow for rarity
+				int rand = Random.Range(0, m_challengeManager.m_challengesCompleated.Count);
+				m_gemIndexNumber = m_challengeManager.m_challengesCompleated[rand];
+			}
+		}
+
+		void ApplyMaterial()
+		{
+			m_ren.material = m_challengeManager.m_allChallenges[m_gemIndexNumber].m_material;
+		}
+
+	}
+}
---
+++ Assets/Scripts/Logic/GemLogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeatRider
+{
+	public class GemLogic : MonoBehaviour
+	{
+		ChallengeManager m_manager;
+		GemTypeRandomizer m_randomizer;
+		
+		// Use this for initialization
+		void Start()
+		{
+			m_manager = FindObjectOfType<ChallengeManager>();
+			m_randomizer = GetComponentInChildren<GemTypeRandomizer>();
+		}
+
+		public void PickupGem()
+		{
+			m_manager.PickupGem(m_randomizer.GetGem());
+		}
+	}
+}
