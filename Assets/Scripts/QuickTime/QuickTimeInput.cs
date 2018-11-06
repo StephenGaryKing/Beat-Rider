36c5021d3e9c7a7ba0c940bdfe2af7ec188738f1
Unknown <thekings.stephen@gmail.com>
2018-11-06 16:53:33 +1030

    Fixed crafting stuff and added quick time stuff

---
+++ Assets/Scripts/QuickTime/QuickTimeInput.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeatRider
+{
+	public class QuickTimeInput : MonoBehaviour
+	{
+
+		public float m_tollerance = 0.2f;
+		public KeyCode[] m_keys = new KeyCode[4];
+
+		Dictionary<KeyCode, float> m_previouslyPressedKeys = new Dictionary<KeyCode, float>();
+		PlayerSoundEffects m_playerSoundEffects;
+		ScoreBoardLogic m_scoreBoardLogic;
+
+		private void Start()
+		{
+			m_playerSoundEffects = GetComponent<PlayerSoundEffects>();
+			m_scoreBoardLogic = FindObjectOfType<ScoreBoardLogic>();
+		}
+
+		private void Update()
+		{
+			if (m_previouslyPressedKeys.Count > 0)
+			{
+				foreach (var key in m_previouslyPressedKeys)
+					Debug.Log(key.Key + " : " + key.Value);
+			}
+			FetchKey();
+			DelKeys();
+		}
+
+		void FetchKey()
+		{
+			int e = System.Enum.GetNames(typeof(KeyCode)).Length;
+			for (int i = 0; i < e; i++)
+			{
+				if (Input.GetKey((KeyCode)i))
+				{
+					if (m_previouslyPressedKeys.ContainsKey((KeyCode)i))
+						m_previouslyPressedKeys[(KeyCode)i] = Time.time;
+					else
+						m_previouslyPressedKeys.Add((KeyCode)i, Time.time);
+				}
+			}
+		}
+
+		void DelKeys()
+		{
+			List<KeyValuePair<KeyCode, float>> valsToDel = new List<KeyValuePair<KeyCode, float>>();
+			foreach (var key in m_previouslyPressedKeys)
+				if (key.Value < Time.time - m_tollerance)
+					valsToDel.Add(key);
+			// del old keypresses
+			foreach (var key in valsToDel)
+				m_previouslyPressedKeys.Remove(key.Key);
+		}
+
+		public IEnumerator LookForKeyPress(int index, GameObject note)
+		{
+			float timer = 0;
+			while (timer < m_tollerance)
+			{
+				List<KeyValuePair<KeyCode, float>> valsToDel = new List<KeyValuePair<KeyCode, float>>();
+				timer += Time.deltaTime;
+				foreach (var key in m_previouslyPressedKeys)
+				{
+					if (key.Key == m_keys[index])
+					{
+						m_scoreBoardLogic.PickupNote();
+						note.SetActive(false);
+						note.GetComponent<ParticleCreationLogic>().SpawnParticle();
+						if (m_playerSoundEffects.m_pickupNote.soundToPlay)
+							m_playerSoundEffects.m_soundManager.PlaySound(m_playerSoundEffects.m_pickupNote);
+						AchievementManager.OnTallyPickups(note.tag);
+						m_previouslyPressedKeys.Remove(key.Key);
+					}
+				}
+				yield return null;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/QuickTime/QuickTimeNote.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BeatRider
+{
+	public class QuickTimeNote : MonoBehaviour {
+
+		int val;
+		public int Val { get { return val; } }
+
+		public Text m_text;
+		QuickTimeInput m_quickTimeInput;
+
+		private void Awake()
+		{
+			m_quickTimeInput = FindObjectOfType<QuickTimeInput>();
+		}
+
+		private void OnEnable()
+		{
+			val = Random.Range(0, 4);
+			m_text.text = m_quickTimeInput.m_keys[val].ToString();
+		}
+	}
+}
