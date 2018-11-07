5cff70e1af684e7c180a2d66247e442e095f69ce
Unknown <thekings.stephen@gmail.com>
2018-11-07 13:16:35 +1030

    Got rid of text on notes (in code)

--- Assets/Scripts/QuickTime/QuickTimeNote.cs
+++ Assets/Scripts/QuickTime/QuickTimeNote.cs
@@ -10,15 +10,16 @@ namespace BeatRider
 		int m_val;
 		public int Val { get { return m_val; } }
 
-		public Text m_text;
 		QuickTimeInput m_quickTimeInput;
 		Renderer m_ren;
 		Color m_defaultColour;
+		float m_immissionIntensity;
 
 		private void Awake()
 		{
 			m_ren = GetComponent<Renderer>();
 			m_defaultColour = m_ren.material.GetColor("_EmissionColor");
+			m_immissionIntensity = GetMag(m_defaultColour);
 			m_quickTimeInput = FindObjectOfType<QuickTimeInput>();
 		}
 
@@ -27,15 +28,11 @@ namespace BeatRider
 			if (m_quickTimeInput.m_gameController.m_difficulty == Difficulty.HARD)
 			{
 				m_val = Random.Range(0, m_quickTimeInput.m_hardKeys.Length);
-				float mag = GetMag(m_ren.material.GetColor("_EmissionColor"));
-				m_ren.material.SetColor("_EmissionColor", m_quickTimeInput.m_hardKeys[m_val].NoteColour * mag);
-				m_text.text = m_quickTimeInput.m_hardKeys[m_val].ToString();
+				m_ren.material.SetColor("_EmissionColor", m_quickTimeInput.m_hardKeys[m_val].NoteColour * m_immissionIntensity);
 			}
 			else if(m_quickTimeInput.m_gameController.m_difficulty == Difficulty.MEDUIM)
 			{
-				float mag = GetMag(m_ren.material.GetColor("_EmissionColor"));
-				m_ren.material.SetColor("_EmissionColor", m_quickTimeInput.m_meduimKey.NoteColour * mag);
-				m_text.text = m_quickTimeInput.m_meduimKey.ToString();
+				m_ren.material.SetColor("_EmissionColor", m_quickTimeInput.m_meduimKey.NoteColour * m_immissionIntensity);
 			}
 			else if (m_quickTimeInput.m_gameController.m_difficulty == Difficulty.EASY)
 				m_ren.material.SetColor("_EmissionColor", m_defaultColour);
