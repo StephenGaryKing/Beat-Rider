ad42730f2114fe5041dc829490e3786c7bdaa5eb
Andrew <lazzyboy1630@gmail.com>
2018-11-20 10:50:33 +1030

    Load main scene script added

---
+++ Assets/Scripts/Logic/GoToNextSceneAfterTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNextSceneAfterTime : MonoBehaviour {
+
+    public float m_waitTime = 5f;
+    float m_timer = 0;
+	
+	// Update is called once per frame
+	void Update () {
+        m_timer += Time.deltaTime;
+        if (m_timer >= m_waitTime)
+            SceneManager.LoadSceneAsync(1);
+	}
+}
