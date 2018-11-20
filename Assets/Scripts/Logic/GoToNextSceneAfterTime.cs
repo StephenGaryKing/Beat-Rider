ea3b0452d00248f3679c63fad1e17b260772f8c3
Unknown <tyler_r_smart@outlook.com>
2018-11-20 12:43:47 +1030

    Intro

--- Assets/Scripts/Logic/GoToNextSceneAfterTime.cs
+++ Assets/Scripts/Logic/GoToNextSceneAfterTime.cs
@@ -7,11 +7,15 @@ public class GoToNextSceneAfterTime : MonoBehaviour {
 
     public float m_waitTime = 5f;
     float m_timer = 0;
+	bool m_going = false;
 	
 	// Update is called once per frame
 	void Update () {
         m_timer += Time.deltaTime;
-        if (m_timer >= m_waitTime)
-            SceneManager.LoadSceneAsync(1);
+		if (!m_going && m_timer >= m_waitTime) 
+		{
+			m_going = true;
+			SceneManager.LoadSceneAsync (1);
+		}
 	}
 }
