2190d6dd4a70f18e5b752fdffbf91d2000f0a4b6
antoine odi <antoine.kenneth.odi@gmail.com>
2019-09-19 13:16:42 +0930

    Mapped Cancel to back button and have volume control for player sound effects such as boost and hit obstacle sound effects

---
+++ Assets/Scripts/UI/BackButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackButton : MonoBehaviour {
+
+    private Button m_backButton = null;
+
+	// Use this for initialization
+	void Start () {
+        m_backButton = gameObject.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_backButton && Input.GetButtonDown("Cancel"))
+            m_backButton.onClick.Invoke();
+	}
+}
