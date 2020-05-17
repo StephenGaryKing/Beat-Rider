dd29a7fb2ea23fe81087bcefb5d94e79036e7851
antoine odi <antoine.kenneth.odi@gmail.com>
2020-05-17 23:45:54 +0930

    Lock and unlock of scenes

---
+++ Assets/Test/Scripts/SceneryManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BeatRider;
+using UnityEngine.UI;
+
+public enum ScenesList
+{
+    Canyon,
+    Forest,
+    Underwater,
+    Blizzard,
+    Sewer,
+    Desert,
+    Highway,
+    Galaxy,
+    Volcano,
+    None
+}
+
+public class SceneryManager : MonoBehaviour {
+    public Dictionary<ScenesList, GameObject> sceneButtons = new Dictionary<ScenesList, GameObject>();
+
+    private void Start()
+    {
+        foreach(KeyValuePair<ScenesList, GameObject> entry in sceneButtons)
+        {
+            entry.Value.GetComponent<Image>().color = Color.gray;
+            entry.Value.GetComponent<Button>().enabled = false;
+        }
+    }
+
+    public void ActivateButton(ScenesList sceneToUnlock)
+    {
+        if (sceneButtons.ContainsKey(sceneToUnlock))
+        {
+            GameObject go = sceneButtons[sceneToUnlock];
+            go.GetComponent<Image>().color = Color.white;
+            go.GetComponent<Button>().enabled = true;
+        }
+    }
+
+}
---
+++ Assets/Test/Scripts/UnlockingScenes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnlockingScenes : MonoBehaviour {
+
+    public ScenesList sceneToUnlock = ScenesList.None;
+    public SceneryManager sceneManager = null;
+
+    private void OnEnable()
+    {
+        sceneManager = FindObjectOfType<SceneryManager>();
+        sceneManager.ActivateButton(sceneToUnlock);
+    }
+}
