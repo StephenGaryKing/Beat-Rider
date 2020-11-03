6e9c6af0c60fb8ce0d6c4d9b7806c3185b818707
Tyler <neon_joystick_tyler@outlook.com>
2020-11-03 12:01:00 +1030

    Demo script additions

---
+++ Assets/Demo/DemoLink.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoLink : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+
+    public void URLOpenA ()
+    {
+        Application.OpenURL("");
+
+    }
+
+    public void URLOpenB ()
+    {
+        Application.OpenURL("https://store.steampowered.com/app/1291720/Neon_Beat_Rider/");
+
+    }
+
+    void Update () {
+		
+	}
+}
