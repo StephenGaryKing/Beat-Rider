3fa57284a3b465e514c542db5ddd4ab6ac76fb22
Tyler <neon_joystick_tyler@outlook.com>
2020-11-11 14:59:49 +1030

    Working on Demo Links (Build Settings changed for demo)

--- Assets/Demo/DemoLink.cs
+++ Assets/Demo/DemoLink.cs
@@ -1,26 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DemoLink : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Button yourButton;
+
+
+    // Use this for initialization
+
+    void Start ()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        Debug.Log("Demolink Script is working...");
+    }
 
     // Update is called once per frame
 
+    public virtual void Select()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=NsGE74GZqz0");
+        Debug.Log("Fish");
+    }
+
     public void URLOpenA ()
     {
-        Application.OpenURL("");
+        Application.OpenURL("https://www.youtube.com/watch?v=NsGE74GZqz0");
 
+    }
+
+    void TaskOnClick()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=NsGE74GZqz0");
+        Debug.Log("Click");
     }
 
     public void URLOpenB ()
     {
-        Application.OpenURL("https://store.steampowered.com/app/1291720/Neon_Beat_Rider/");
-
+        // Application.OpenURL("https://www.youtube.com/watch?v=NsGE74GZqz0");
+        Debug.Log("Fish");
     }
 
     void Update () {
